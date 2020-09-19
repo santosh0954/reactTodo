f649122869690f906b043c3de8aabe24c1adafea
santosh <santosh0954jha@gmail.com>
2020-09-19 20:44:41 +0530

    Updates

---
+++ precache-manifest.b5abfe569185ffa3fb9eb6408187e23e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985db209bf11f109354f17f929a6f045",
+    "url": "/index.html"
+  },
+  {
+    "revision": "add0babddaebc2b2ecdc",
+    "url": "/static/css/main.42d721a1.chunk.css"
+  },
+  {
+    "revision": "879e2c4f79de53894415",
+    "url": "/static/js/2.22cc9126.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.22cc9126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add0babddaebc2b2ecdc",
+    "url": "/static/js/main.182fe40a.chunk.js"
+  },
+  {
+    "revision": "97e8a9313bd740645a10",
+    "url": "/static/js/runtime-main.42a11002.js"
+  }
+]);
